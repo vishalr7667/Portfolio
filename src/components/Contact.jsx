5518e0757481fe5c753ed6e922a0c8e774b20750
Vishal Kumar <76913781+vishalr7667@users.noreply.github.com>
2026-01-14 21:34:48 +0530

    Update Web3Forms access key in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -53,7 +53,7 @@ export default function Contact() {
 
     // Create a new FormData object to send to Web3Forms API
     const form = new FormData();
-    form.append("access_key", "90f4b8af-e590-42b0-beaf-10b18f66a703"); // Replace with your Web3Forms access key
+    form.append("access_key", "b356009d-b38a-40eb-b0e3-bb8eafebc51a");
     form.append("name", formData.name);
     form.append("email", formData.email);
     form.append("subject", formData.subject || "New Contact Form Submission");
